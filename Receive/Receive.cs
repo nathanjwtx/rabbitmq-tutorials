92a686d2ebdf258fe5559abe87b57d6d31d8567a
nathanjwtx <nathan@nathanjwtx.io>
2019-06-26 14:29:03 -0500

    Completed Tutorial 2

--- Receive/Receive.cs
+++ Receive/Receive.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.Threading;
 
 namespace Receive
 {
@@ -9,7 +10,48 @@ namespace Receive
     {
         public static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+//            Tutorial1();
+            Tutorial2();
+        }
+
+        public static void Tutorial2()
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        int dots = message.Split(".").Length - 1;
+                        Thread.Sleep(dots * 1000);
+                        
+                        Console.WriteLine($" [x] received {message}. Waited for {dots} seconds");
+                    };
+
+                    channel.BasicConsume(queue: "task_queue",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [Enter] to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Tutorial1()
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
@@ -19,7 +61,7 @@ namespace Receive
                         exclusive: false,
                         autoDelete: false,
                         arguments: null);
-                    
+
                     var consumer = new EventingBasicConsumer(channel);
                     consumer.Received += (model, ea) =>
                     {
--- Send/Send.cs
+++ Send/Send.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using RabbitMQ.Client;
 using System.Text;
+using System.Threading;
 
 namespace Send
 {
@@ -8,7 +9,49 @@ namespace Send
     {
         public static void Main(string[] args)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+//            Tutorial1();
+            Tutorial2(args);
+        }
+
+        public static void Tutorial2(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    var properites = channel.CreateBasicProperties();
+                    properites.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "task_queue",
+                        basicProperties: properites,
+                        body: body);
+
+                    Console.WriteLine($" [x] sent {message}");
+                }
+            }
+
+//            Console.WriteLine("Press [Enter] to exit");
+//            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World");
+        }
+        
+        private static void Tutorial1()
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
@@ -21,7 +64,7 @@ namespace Send
 
                     string message = "Hello World!";
                     var body = Encoding.UTF8.GetBytes(message);
-                    
+
                     channel.BasicPublish(exchange: "",
                         routingKey: "hello",
                         basicProperties: null,
@@ -33,6 +76,6 @@ namespace Send
 
             Console.WriteLine("Press [Enter] to exit");
             Console.ReadLine();
-        } 
+        }
     }
 }
